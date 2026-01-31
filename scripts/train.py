db0163cc301b9a7b91418ecc4bde9b8f122847b5
ZhouLon <1922450589@qq.com>
2026-02-01 02:31:22 +0800

    update embedding

--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,13 @@
 from src.data.load_data import load_table
+from src.data.embedding
 from pathlib import Path
 
 
 if __name__=="__main__":
     file_path=Path(r'data\DMS_ProteinGym_substitutions\A0A1I9GEU1_NEIME_Kennouche_2019.csv')
-    f,l=load_table(file_path=file_path,feature_index=1,label_index=2,header_index=0)
-    print(f,l)
+    featrue,label=load_table(file_path=file_path,feature_index=1,label_index=2,header_index=0)
+
+    嵌入
+
+
+    
